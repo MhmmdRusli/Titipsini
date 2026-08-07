f5569effbd1a960c1c1f0daf23b3570c6e8fa5ca
apip477 <apippp197@gmail.com>
2026-08-07 10:09:08 +0700

    ok

--- routes/web.php
+++ routes/web.php
@@ -75,7 +75,7 @@ Route::get('/', function () {
     return Inertia::render('Onboarding');
 })->name('welcome');
 
-// Auth routes
+
 Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
     Route::post('/login', [AuthenticatedSessionController::class, 'store']);
